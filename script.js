dd13c001cd297e1cde9a1c441682ed7c40d2906d
Kovtun Pavlo <covtynpavlo70@gmail.com>
2025-09-26 16:13:03 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const navLinks = document.querySelectorAll(".nav-grid a");
+
+// navLinks.forEach(link => {
+//   link.addEventListener("click", function (e) {
+//     navLinks.forEach(l => l.classList.remove("active"));
+//     this.classList.add("active");
+//   });
+// });
