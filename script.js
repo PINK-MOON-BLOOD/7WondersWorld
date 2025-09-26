5353a6a26d6f9527540c568bb4bfac0fc03d6b65
Kovtun Pavlo <covtynpavlo70@gmail.com>
2025-09-26 16:27:19 +0300

    delete one fail

--- script.js
+++
@@ -1,8 +0,0 @@
-// const navLinks = document.querySelectorAll(".nav-grid a");
-
-// navLinks.forEach(link => {
-//   link.addEventListener("click", function (e) {
-//     navLinks.forEach(l => l.classList.remove("active"));
-//     this.classList.add("active");
-//   });
-// });
